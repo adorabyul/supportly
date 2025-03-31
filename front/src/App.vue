afebef832162490706aa3bf86d6b8edca093b4f5
adorabyul <williamcarlstrom@hotmail.com>
2025-03-31 02:51:57 +0200

    added disclaimer, author and description

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,12 +5,17 @@ import Chat from "./components/Chat.vue";
 
 
 <template>
-  <h1>Supportly</h1>
+  <div class="title">
+      <h1>Supportly</h1>
+      <p class="disclaimer">Disclaimer: This is not a real customer support chatbot and is only built as a proof of concept, read more <a class="link" href="https://github.com/adorabyul/supportly">here.</a></p>
+  </div>
+  
     <Chat/>
 </template>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap");
   body {
     margin: 0;
     padding: 0;
@@ -32,6 +37,30 @@ import Chat from "./components/Chat.vue";
     font-size: 3em;
     font-style: normal;
 
+  }
+  
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .disclaimer {
+    color: white;
+    font-family: "Roboto Flex", sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 350;
+    max-width: 18em;
+    font-style: normal;
+  }
+
+  .link {
+    color: #4663ac;
+  }
+
+  .link:visited {
+    color: #4663ac;
   }
 
 </style>
