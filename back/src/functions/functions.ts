e6be90de14cbc548ca959c3794a8a0228a7aae9a
adorabyul <williamcarlstrom@hotmail.com>
2025-03-31 02:17:09 +0200

    formatting

--- back/src/functions/functions.ts
+++ back/src/functions/functions.ts
@@ -22,15 +22,15 @@ async function getEmbedding(question: string) {
     }
   }
   
-  function cosineSimilarity(vec1: number[], vec2: number[]): number {
+function cosineSimilarity(vec1: number[], vec2: number[]): number {
     const dotProduct = vec1.reduce((acc, val, i) => acc + val * vec2[i], 0);
     const norm1 = Math.sqrt(vec1.reduce((acc, val) => acc + val * val, 0));
     const norm2 = Math.sqrt(vec2.reduce((acc, val) => acc + val * val, 0));
     
     return dotProduct / (norm1 * norm2);
-  }
+}
 
-  async function getChatGptResponse(question: string): Promise<string | null> {
+async function getChatGptResponse(question: string): Promise<string | null> {
   
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
@@ -49,9 +49,9 @@ async function getEmbedding(question: string) {
     });
   
     return response.choices[0].message.content;
-  }
+}
   
-  export async function findMostSimilarFaq(question: string): Promise<string | null> {
+export async function findMostSimilarFaq(question: string): Promise<string | null> {
     const userEmbedding = await getEmbedding(question);
     
     if (!userEmbedding) {
@@ -69,14 +69,14 @@ async function getEmbedding(question: string) {
         highestSimilarity = similarity;
         mostSimilarFaq = faq;
       }
-    });
+});
   
   
-    const similarityThreshold = 0.8; 
+const similarityThreshold = 0.8; 
     if (highestSimilarity >= similarityThreshold && mostSimilarFaq) {
       return mostSimilarFaq.answer;
     }
   
     return await getChatGptResponse(question);
-  }
+}
  
