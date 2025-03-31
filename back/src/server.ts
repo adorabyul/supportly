84f7b5ee7c9c41da3e6aec0c749b624c03d89803
adorabyul <williamcarlstrom@hotmail.com>
2025-03-31 02:18:23 +0200

    added multer and api endpoint for transcription of audio messages using whisper

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { WebSocketServer } from "ws";
 import { findMostSimilarFaq } from "./functions/functions";
+import fs from "fs";
+import multer from "multer";
+import OpenAI from "openai";
+import { Transcription } from "openai/resources/audio/transcriptions";
+import path from "path";
 
 dotenv.config();
 
@@ -11,9 +16,26 @@ const wss = new WebSocketServer({ port: 5000 });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (_req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage });
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+
 
 
 wss.on("connection", (ws) => {
@@ -31,6 +53,33 @@ wss.on("connection", (ws) => {
       console.log("Client disconnected");
     });
   });
+
+
+app.post("/transcribe", upload.single("audio"), async (req: Request, res: Response) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No audio file uploaded" });
+    }
+
+    const audioPath = req.file.path;
+    console.log(audioPath);
+    console.log(req.file.mimetype)
+
+    const response: Transcription = await openai.audio.transcriptions.create({
+      file: fs.createReadStream(audioPath),
+      model: "whisper-1",
+    });
+
+    const botReply: any = await findMostSimilarFaq(response.text);
+
+    res.json({ text: botReply, transcription: response });
+
+    fs.unlinkSync(audioPath);
+  } catch (error) {
+    console.error("Error during transcription:", error);
+    res.status(500).json({ error: error instanceof Error ? error.message : "Internal Server Error" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
